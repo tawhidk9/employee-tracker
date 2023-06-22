2d7310dc4c403d987ea65e80f8bdecd75e9bba55
Tawhid <tawhidkamal5@gmail.com>
2023-06-21 22:06:45 -0400

    added new department funciton in index

--- index.js
+++ index.js
@@ -19,7 +19,26 @@ const roleQ = db.query(`SELECT * FROM role`,(err,result) => {
 
 //Employee Query
 const employeeQ = db.query(`SELECT * FROM employee`,(err,result) => {
-    if (err) {console.log(err)}
+    if (err) {console.log(err);}
     console.table(result);
 });
 
+// Add Department
+
+const newDepartment = () => {
+    const question = {
+      type: "input",
+      message: "What is the name of the new department?",
+      name: "deptName",
+    };
+    inquirer.prompt(question).then((answer) => {
+        console.log(answer);
+      db.query(`INSERT INTO department (name) VALUES (?)`, 
+      answer.deptName,
+      (err, result)=> {if (err) {console.log(err);}
+          departmentsQuery();
+        });
+    });
+  };
+
+  
