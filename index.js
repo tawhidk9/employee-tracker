1375db475e0aceb2973be1e4786e0c15b85f8bc3
Tawhid <tawhidkamal5@gmail.com>
2023-06-21 22:40:37 -0400

    added handleanswer functrion

--- index.js
+++ index.js
@@ -6,19 +6,19 @@ const cTable = require('console.table');
 //QUERIES//
 
 //Department Query
-const deptQ = db.query(`SELECT * FROM department`,(err,result) => {
+const deptQ = () => db.query(`SELECT * FROM department`,(err,result) => {
     if (err) {console.log(err)}
     console.table(result);
 });
 
 //Role Query
-const roleQ = db.query(`SELECT * FROM role`,(err,result) => {
+const roleQ = () => db.query(`SELECT * FROM role`,(err,result) => {
     if (err) {console.log(err)}
     console.table(result);
 });
 
 //Employee Query
-const employeeQ = db.query(`SELECT * FROM employee`,(err,result) => {
+const employeeQ = () => db.query(`SELECT * FROM employee`,(err,result) => {
     if (err) {console.log(err);}
     console.table(result);
 });
@@ -57,4 +57,26 @@ const newDepartment = () => {
     name: "menu",
   };
 
+//Depending on what User selects, it should trigger different queries. switch statement? if/elif/then?
+
+function handleAnswers(answers) {
+    const selectedAnswer = answers.name;
   
+    // console.log(selectedAnswer);
+    switch (selectedAnswer) {
+      case "View Departments":
+        deptQ();
+        break;
+      case "View Roles":
+        roleQ();
+        break;
+      case "View Employees":
+        employeeQ();
+        break;
+      case "Add Department":
+        newDepartment();
+        break;
+    }
+  }
+  
+  inquirer.prompt(mainMenu).then((answers) => handleAnswers(answers));
