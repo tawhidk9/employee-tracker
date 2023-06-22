077c1f9a373b19fd3dd1f54a5d7eb646a8f94c4e
Tawhid <tawhidkamal5@gmail.com>
2023-06-21 21:54:08 -0400

    added dependencies, created queries in index.js, added cTable as well

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
+//DEPENDENCIES
 const inquirer = require("inquirer");
+const db = require("./db/db");
+const cTable = require('console.table');
+
+//QUERIES//
+
+//Department Query
+const deptQ = db.query(`SELECT * FROM department`,(err,result) => {
+    if (err) {console.log(err)}
+    console.table(result);
+});
+
+//Role Query
+const roleQ = db.query(`SELECT * FROM role`,(err,result) => {
+    if (err) {console.log(err)}
+    console.table(result);
+});
+
+//Employee Query
+const employeeQ = db.query(`SELECT * FROM employee`,(err,result) => {
+    if (err) {console.log(err)}
+    console.table(result);
+});
+
